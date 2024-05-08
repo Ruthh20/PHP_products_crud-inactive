27f1cb053e2850f3a8cae544e881ad9c437ecc3f
Ruth <90890343+Ruthh20@users.noreply.github.com>
2024-05-08 23:05:41 +0530

    Update create_products.php

---
+++ create_products.php
@@ -0,0 +1,110 @@
+<?php 
+require_once "functions.php";
+// to create connection to db
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+// save data in mysql by post method
+
+$errors = [];
+
+$title = '';
+$description = '';
+$price = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+
+    if ($image && $image['tmp_name']) {
+        $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+        mkdir(dirname($imagePath));
+        move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (empty($errors)) {
+        $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                VALUES (:title, :image, :description, :price, :date)");
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', $imagePath);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', date('Y-m-d H:i:s'));
+
+        $statement->execute();
+        header('Location: index.php');
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products CRUD</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    
+ <!----------------- table ------------------------>
+ <h1>Create New Product</h1>
+
+ <?php if (!empty($errors)): ?>
+    <div class="alert alert-danger">
+        <?php foreach ($errors as $error): ?>
+            <div><?php echo $error ?></div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+
+<!------- form for creating new product --------->
+
+<form method="post" enctype="multipart/form-data">
+
+    <div class="form-group">
+        <label>Product Image</label><br>
+        <input type="file" name="image">
+    </div>
+
+    <div class="form-group">
+        <label>Product title</label>
+        <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+    </div>
+
+    <div class="form-group">
+        <label>Product description</label>
+        <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+    </div>
+
+    <div class="form-group">
+        <label>Product price</label>
+        <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</body>
+</html>
